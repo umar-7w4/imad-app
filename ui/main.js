2408cdc228ae9156671c5bc2ffb3851c7ad879f7
umar-7w4 <30959585+umar-7w4@users.noreply.github.com>
2017-08-16 17:24:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 
 // move image
 var img = document.getElementById("mohammad umar");
-var marginleft = 0;
+var marginleft = 10;
 function marginright () {
     marginleft = marginleft + 10;
     img.style.marginleft = marginleft + "px";
