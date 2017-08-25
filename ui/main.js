bb07d1dddb75e57db7a82518ad5348bcfe004bf2
umar-7w4 <30959585+umar-7w4@users.noreply.github.com>
2017-08-25 23:16:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,21 +5,22 @@ var counter = 0;
 button.onclick = function () {
     
      // create a request object
-     var request= new XMLhttpRequest();
-     
+     var request = new XMLhttpRequest();
+      
      // capture the response and store it in the variable
      request.onreadystatechange = function () {
+         if (request.readystate === XMLhttprequest.DONE) {
          // take some action
          if ( request.status === 200) {
-             var counter= request.response.text;
+              var counter = request.response.text;
               var span = document.getElementById('count');
-     span.innerHTML = counter.toString();
+              span.innerHTML = counter.toString();
          }
-     };
+    }
      // not done yet
 }; 
 
 // make a request
 request.open('GET', 'http://mohammadumar7w4.imad.hasura-app.io/',true);
 request.send(null);
-
+};
