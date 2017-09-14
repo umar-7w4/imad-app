24f6237e9778188530186a59d07bc98c8a41a68e
umar-7w4 <30959585+umar-7w4@users.noreply.github.com>
2017-09-14 17:12:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -29,19 +29,35 @@ var nameInput= document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById("submit_button");
 submit.onclick = function () {
-    // make a rquest to the server and send the name
-    
+    // create a request object
+     var request = new XMLhttpRequest();
+      
+      
+     // capture the response and store it in the variable
+     request.onreadystatechange = function () {
+         if (request.readystate === XMLhttprequest.DONE) {
+         // take some action
+         if ( request.status === 200) {
     // capture the list of name and render the list
-    var names=["name1", "name2", "name3", "name4"];
+    var names = request.responseText;
+    names = JSON.parse(names);
     var list = "";
     for (var i=0; i< names.length; i++) {
         list == "<li>" + name[i] + "<li>";
     }
     var ul = document.getElementById("namelist");
     ul.innerHTML = list;
+         }    
+         }
+         // Not done yet
+     };
+     
+     // make the request
+     request.open('Get', 'http://mohammadumar7w4.imad.hasura-app.io/submit-name?name=' + name, true);
+     request.send(null);
+     
 };
     
     
     
-}
     
