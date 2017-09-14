74e08b43461e11de420bba50004d2f3808dc6854
umar-7w4 <30959585+umar-7w4@users.noreply.github.com>
2017-09-14 16:41:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/ui/madi.png', function (req, res) {
 
 
 var names = [];
-app.get('/submit-name/:name' , function(req, res) { // /submit-name?name=xxxx 
+app.get('/submit-name/:name' , function(req, res) { 
     // Get the name from request 
     var name = req.params.name;
     
